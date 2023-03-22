799eaa4c9ab36bb6b38cf8f8507e9dc2b2e59f2e
ljy2855 <ljy2855@naver.com>
2023-03-22 18:30:21 +0900

    feat: Add parse command

--- prj1/myshell.c
+++ prj1/myshell.c
@@ -2,16 +2,51 @@
 
 
 int main(){
-    char command[100];
+    char command[MAX_COMMAND_LENGTH];
+    char ** args;
     do {
         printf("CSE4100:P1-myshell>");
-        scanf("%s",command);
+        fgets(command,MAX_COMMAND_LENGTH,stdin);
+        args = parse_command(command);
+        execute_command(args);
 
-        
-    }
+        free(args);
+    }while (1);
+    
 }
 
-char** parse_command(char[] command){
+char** parse_command(char command[]){
+    char** result;
+    char * temp;
+    int count = 0;
+
+    result = (char**)malloc(SIZE_OF_CHAR_POINTER);
+    temp = strtok(command," ");
+    result[count] = temp;
+
+    count++;
+    while(temp != NULL){
+        result = realloc(result,SIZE_OF_CHAR_POINTER * (count+1));
+        temp = strtok(NULL," ");
+        result[count] = temp;
+        // strcpy(result[count],temp);
+        count++;
+
+    }
 
 
+    return result;
+}
+
+void execute_command(char ** args){
+    char path[1024];
+    __pid_t pid;
+    getcwd(path,sizeof(path));
+    if(pid = fork() == 0){
+        //child process
+        execlp(path, args[0],NULL);
+    }else{
+        //parent process
+        wait();
+    }
 }
--- prj1/myshell.h
+++ prj1/myshell.h
@@ -1,5 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include<string.h>
 
-char[] * parse_command(char[] command);
+#define MAX_ARGS 100
+#define MAX_COMMAND_LENGTH 200
+#define SIZE_OF_CHAR_POINTER sizeof(char*)
+
+char** parse_command(char command[]);
+void execute_command(char ** args);
