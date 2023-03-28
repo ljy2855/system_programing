b6640809b5a0266e16ccb962687d832de2acade6
ljy2855 <ljy2855@naver.com>
2023-03-28 17:46:44 +0900

    feat: Implement history command

--- prj1/phase1/myshell.c
+++ prj1/phase1/myshell.c
@@ -3,22 +3,85 @@
 int main(){
     char command[MAX_COMMAND_LENGTH];
     char ** args;
-    do {
-        printf("CSE4100:P1-myshell>");
+    read_bash_history();
+    do
+    {
+        printf("CSE4100-MP-P1>");
         fgets(command,MAX_COMMAND_LENGTH,stdin);
         if(command[0] == '\n')
             continue;
         command[strcspn(command, "\n")] = 0;
-        args = parse_command(command);
-        execute_command(args);
+        if(!check_history(command)){
+            args = parse_command(command);
+            execute_command(args);
+            free(args);
+        }
 
-        free(args);
-    }while (1);
-    
+    } while (1);
+}
+int read_bash_history(){
+    fp = fopen(".bash_history", "r");
+    char command[MAX_COMMAND_LENGTH] = {0,};
+    char *temp;
+
+    while(!feof(fp)){
+        fgets(command, MAX_COMMAND_LENGTH, fp);
+        if (command[0] == '\n')
+            continue;
+        command[strcspn(command, "\n")] = 0;
+        add_command_history(command, 0);
+        //printf("%s", command_history[history_count - 1]);
+    }
+    fclose(fp);
+}
+void add_command_history(char command[],int write_file){
+    history_count++;
+    command_history = (char **)realloc(command_history, sizeof(char *) * history_count);
+    command_history[history_count - 1] = (char *)malloc(sizeof(char) * MAX_COMMAND_LENGTH);
+    strcpy(command_history[history_count - 1], command);
+    if(write_file){
+        
+        fp = fopen(".bash_history", "a");
+       
+        fprintf(fp, "\n%s", command);
+        fclose(fp);
+    }
 }
 
-// TODO
-// build in command ex) quit,exit
+int check_history(char command[]){
+    int i =0;
+    int target = 0;
+    if(strcmp(command,"history") == 0){
+        for(;i<history_count;i++){
+            printf("%d\t%s\n",i+1,command_history[i]);
+        }
+
+        add_command_history(command, 1);
+        return 1;
+    }else if(command[0] == '!'){
+        if(command[1] == '!'){
+            printf("%s\n",command_history[history_count-1]);
+            target = history_count - 1;
+        }else if(atoi(command+1) != 0){
+            if(atoi(command+1) <= history_count){
+                target = atoi(command+1) -1;
+                printf("%s\n",command_history[target]);
+                add_command_history(command_history[target], 1);
+            }
+
+            else
+            {
+                printf("out of index history\n");
+                return 1;
+            }
+            
+        }
+        strcpy(command,command_history[target]);
+        return 0;
+    }
+    add_command_history(command, 1);
+    return 0;
+}
 
 char** parse_command(char command[]){
     char** result;
@@ -47,29 +110,59 @@ void execute_command(char ** args){
     int status;
     __pid_t pid;
 
-    if(strcmp(args[0],"cd") == 0){
-        chdir(args[1]);
-    }else if(strcmp(args[0],"exit") == 0){
-        exit(EXIT_SUCCESS);
-    }
-    else{
-        if(fork() == 0){
-            //child process
-            //printf("%s\n",command);
-            if(execvp(args[0], args) < 0 ){
-                printf("%s: Command not found.\n", args[0]);
-                exit(0);
-            }
+    if(execute_excp_command(args)){
+        return;
 
-        }
+    }
         else
         {
-            //parent process
-            
-            wait(&status);
-            if (WEXITSTATUS(status)) 
-                exit(1);
-        
+            if (fork() == 0)
+            {
+                // child process
+                // printf("%s\n",command);
+                if (execvp(args[0], args) < 0)
+                {
+                    printf("%s: Command not found.\n", args[0]);
+                    exit(0);
+                }
+            }
+            else
+            {
+                // parent process
+
+                wait(&status);
+                if (WEXITSTATUS(status))
+                    exit(1);
+            }
         }
+}
+
+int execute_excp_command(char **args)
+{
+        int i = 0;
+        if (strcmp(args[0], "cd") == 0)
+        {
+            chdir(args[1]);
+            return 1;
     }
+    else if (strcmp(args[0], "exit") == 0)
+    {
+            for (; i < history_count;i++)
+            {
+                free(command_history[i]);
+            }
+            free(command_history);
+            exit(1);
+    }
+    else if (strcmp(args[0], "history") == 0)
+    {
+        for (; i < history_count-1; i++)
+        {
+                printf("%d\t%s\n", i + 1, command_history[i]);
+        }
+        //strcpy(command_history[history_count++], command);
+        return 1;
+    }
+
+    return 0;
 }
--- prj1/phase1/myshell.h
+++ prj1/phase1/myshell.h
@@ -8,6 +8,14 @@
 #define MAX_ARGS 100
 #define MAX_COMMAND_LENGTH 200
 #define SIZE_OF_CHAR_POINTER sizeof(char*)
+#define MAX_HISTORY_COUNT 100
+char ** command_history;
+int history_count = 0;
+FILE * fp;
 
 char** parse_command(char command[]);
 void execute_command(char ** args);
+int check_history(char command[]);
+int execute_excp_command(char **args);
+int read_bash_history();
+void add_command_history(char command[], int write_file);
