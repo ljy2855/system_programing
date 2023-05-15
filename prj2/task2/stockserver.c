42d4845fd4ca3f0b24deeae8da1d0ce7423f097b
ljy2855 <ljy2855@naver.com>
2023-05-15 20:28:41 +0900

    feat: Implement multi thread base server without lock

--- prj2/task2/stockserver.c
+++ prj2/task2/stockserver.c
@@ -10,18 +10,22 @@
 
 void echo(int connfd);
 void init_stock();
+void * connect_client(void *arg);
+char * process_request(char command[]);
 
 void terminate_handler(int sig);
 int main(int argc, char **argv)
 {
-    int listenfd, connfd;
+    int listenfd,* connfd;
     socklen_t clientlen;
     struct sockaddr_storage clientaddr;  /* Enough space for any address */  //line:netp:echoserveri:sockaddrstorage
     char client_hostname[MAXLINE], client_port[MAXLINE];
+    pthread_t t_id;
 
     Signal(SIGINT,terminate_handler);
 
     init_stock();
+    
 
     if (argc != 2) {
 	fprintf(stderr, "usage: %s <port>\n", argv[0]);
@@ -33,17 +37,39 @@ int main(int argc, char **argv)
 
     while (1){
    
-      
+        connfd = (int*)malloc(sizeof(int));
         clientlen = sizeof(struct sockaddr_storage);
-        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        *connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
         Getnameinfo((SA *)&clientaddr, clientlen, client_hostname, MAXLINE,
                     client_port, MAXLINE, 0);
         printf("Connected to (%s, %s)\n", client_hostname, client_port);
+        pthread_create(&t_id,NULL,connect_client,connfd);
 
-        
     }
     
     exit(0);
+}
+void * connect_client(void *arg){
+    int connfd = *((int*) arg);
+    int n; 
+    char buf[MAXLINE]; 
+    char * response;
+    rio_t rio;
+    pthread_detach(pthread_self());
+    
+    
+
+    Rio_readinitb(&rio, connfd);
+    while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
+        printf("server received %d bytes\n", n);
+        
+        response = process_request(buf);
+        Rio_writen(connfd, response, MAXLINE);
+        free(response);
+    }
+    close(connfd);
+    Free(arg);
+    return NULL;
 }
 
 
